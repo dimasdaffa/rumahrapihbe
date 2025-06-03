0780d51ec96fd5c07aab87e53a86058f7ea9421c
dimasdaffa <dimas.de2004@gmail.com>
2025-06-03 10:36:29 +0700

    Add model & install filament

--- app/Models/HomeService.php
+++ app/Models/HomeService.php
@@ -3,8 +3,46 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Stringable;
 
 class HomeService extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'price',
+        'category_id',
+        'is_popular',
+        'duration',
+    ];
+
+    //Mutator for the 'slug' attribute
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    //Banyak data realis dalam 1 tabel
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(ServiceBenefit::class);
+    }
+
+    public function testimonials(): HasMany
+    {
+        return $this->hasMany(ServiceTestimonial::class);
+    }
+    //Relasi ke tabel kategori 1 aja
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
--- app/Models/ServiceTestimonial.php
+++ app/Models/ServiceTestimonial.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ServiceTestimonial extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'message',
+        'photo',
+        'home_service_id',
+    ];
+
+    public function homeService(): BelongsTo
+    {
+        return $this->belongsTo(HomeService::class, 'home_service_id');
+    }
 }
